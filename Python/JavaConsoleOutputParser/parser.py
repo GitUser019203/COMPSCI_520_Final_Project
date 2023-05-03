bbe1ed32170745f493f62539d15f69c03081e541
mathk <mathk@LAPTOP-R5HJL886>
2023-05-03 09:41:19 -0400

    Formatted the output that the exception parser module generates.

--- Python/JavaConsoleOutputParser/parser.py
+++ Python/JavaConsoleOutputParser/parser.py
@@ -18,24 +18,25 @@ with open("Python/JavaConsoleOutputParser/extracted_errors", 'w') as out_file:
             else:
                 error_list.append(trace_dict)
                 trace_dict = {}
-        for trace_dict in error_list:
-            if trace_dict:
-                found_error_info = False
-                print('\n', file=out_file)
-                for key in trace_dict:
-                    print(f"{key}: {trace_dict[key].strip()}", file=out_file)
-                for key in trace_dict:
-                    line_number = int(key)
-                    i = 1
-                    while(not found_error_info):
-                        if re.search(error_info_pattern, lines[line_number - i]):
-                            print(f"Line about exception that occurred: {lines[line_number - i].strip()}", file=out_file)
-                        if re.search(parsed_line_pattern, lines[line_number - i]):
-                            print(f"Line that was being parsed: {lines[line_number - i].strip().replace('Currently', 'Was')}", file=out_file)
-                            found_error_info = True
-                        if re.search(revison_hash_pattern, lines[line_number - i]):
-                            print(f"Line about revision hash that wasn't mined': {lines[line_number - i].strip()}", file=out_file)
-                        i += 1
-                        if line_number - i <= 1:
-                            break
-                print('\n', file=out_file)
+        traces = [trace_dict for trace_dict in error_list if trace_dict]
+        print(f"There were at least {len(traces)} exceptions that occurred.", file=out_file)
+        for trace_dict in traces:
+            found_error_info = False
+            print('\n', file=out_file)
+            for key in trace_dict:
+                print(f"{key}: {trace_dict[key].strip()}", file=out_file)
+            for key in trace_dict:
+                line_number = int(key)
+                i = 1
+                while(not found_error_info):
+                    if re.search(error_info_pattern, lines[line_number - i]):
+                        print(f"Line about exception that occurred: {lines[line_number - i].strip()}", file=out_file)
+                    if re.search(parsed_line_pattern, lines[line_number - i]):
+                        print(f"Line that was being parsed: {lines[line_number - i].strip().replace('Currently', 'Was')}", file=out_file)
+                        found_error_info = True
+                    if re.search(revison_hash_pattern, lines[line_number - i]):
+                        print(f"Line about revision hash that wasn't mined': {lines[line_number - i].strip()}", file=out_file)
+                    i += 1
+                    if line_number - i <= 1:
+                        break
+            print('\n', file=out_file)
