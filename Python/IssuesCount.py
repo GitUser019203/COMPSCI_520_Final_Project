7dcf47a09febafbd46bb69f6f1a52d25cfed8b09
PYee1999 <prestonkyee@gmail.com>
2023-05-08 16:54:46 -0400

    Resolving merge conflict with IssuesCount.py in the Python folder

--- Python/IssuesCount.py
+++ Python/IssuesCount.py
@@ -1,9 +1,43 @@
 
 
+<<<<<<< HEAD
 
 def main():
     
     return
+=======
+def countTotalIssueTitles(file_path):
+    """
+    Counts grand total number of Issue Titles in .txt file
+    """
+    # IssueTitles Counter
+    total_num_issue_titles = 0
+
+    # Access .txt file
+    with open(file=file_path, mode='r', encoding="utf-8") as out_file:
+        for line in out_file:
+            if "Issue Title:" in line:
+                total_num_issue_titles += 1
+
+    # Return total number of issue titles
+    return total_num_issue_titles
+
+
+def countIssueTitlesContaining():
+    """
+    Counts grand total number of issues containing specific words relating to a key word.
+    """
+    return 0
+
+
+def main():
+    """Main Method"""
+    # Calculate total number of issue titles
+    total_iss = countTotalIssueTitles(file_path="/Users/preston/CS520_Final_Project/COMPSCI_520_Final_Project/Python/mongo_db_extract_refactoring_doc.txt")
+    print("TOTAL_ISSUE_TITLES:", total_iss)
+>>>>>>> c540d70 (Code cleanup for IssuesCount.py)
+
+    # Later: We calculate total number of issue titles containing key words or related words
 
 if __name__ == "__main__":
     main()
