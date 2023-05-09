3441dd5b77d630a7d8586a6f5d14b38089c8aa3a
PYee1999 <prestonkyee@gmail.com>
2023-05-09 14:49:24 -0400

    Added related-key words and calculated RQ2 percentage

--- Python/IssuesCount.py
+++ Python/IssuesCount.py
@@ -44,16 +44,72 @@ def main():
 
     # Calculate total number of issue titles
     total_iss = countTotalIssueTitles(file_path=file_path_test)
-    print("Grand Total Issue Titles:", total_iss)
 
-    # Later: We calculate total number of issue titles containing key words or related words
+    # Calculate total number of issue titles containing key words ("bugs", "refactoring")
     bug_keywords = ["bugs", "bug"]
     total_iss_bugs = countIssueTitlesContaining(file_path=file_path_test, key_word_list=bug_keywords)
-    print("Total 'bugs' Issue Titles:", total_iss_bugs)
 
     refactor_keywords = ["refactor", "refactoring", "refactors", "refactorings", "refactored"]
     total_iss_refactor = countIssueTitlesContaining(file_path=file_path_test, key_word_list=refactor_keywords)
+
+    # Caculate total number of issue title containing key words or words similar to key word
+    bug_keywords_related = ["bug", "bugs", "error", "failure", "defect", "fault"]
+    total_related_iss_bugs = countIssueTitlesContaining(file_path=file_path_test, key_word_list=bug_keywords_related)
+
+    refactor_keywords_related = [
+        "add", 
+        "chang" , 
+        "clean",
+        "code optimization",
+        "creat",
+        "extend",
+        "extract",
+        "fix",
+        "improv",
+        "inlin",
+        "introduc",
+        "merg",
+        "mov",
+        "pull up", 
+        "pulls up",
+        "pulling up", 
+        "pulled up",
+        "push down", 
+        "pushes down", 
+        "pushed down", 
+        "pushing down",
+        "repackag",
+        "redesign", 
+        "reduc", 
+        "refactor", 
+        "refin", 
+        "remov", 
+        "renam", 
+        "reorganiz", 
+        "replac", 
+        "restructur",
+        "rewrit",
+        "simplify",
+        "simplifi",
+        "split"
+    ]
+    total_related_iss_refactor = countIssueTitlesContaining(file_path=file_path_test, key_word_list=refactor_keywords_related)
+    
+    # Calculate percentages
+    refactor_percent = (total_iss_refactor / total_related_iss_refactor) * 100
+    bug_percent = (total_iss_bugs / total_related_iss_bugs) * 100
+    
+    print("----- Research Question 2 -----")
+    print("Grand Total Issue Titles:", total_iss)
+    print()
+    print("Total 'bugs' Issue Titles:", total_iss_bugs)
+    print("Total 'bug-related' Issue Titles:", total_related_iss_bugs)
+    print()
     print("Total 'refactor' Issue Titles:", total_iss_refactor)
+    print("Total 'refactor-related' Issue Titles:", total_related_iss_refactor)
+    print()
+    print("Bug Percentage:", bug_percent)
+    print("Refactor Percentage:", refactor_percent)
 
 if __name__ == "__main__":
     main()
