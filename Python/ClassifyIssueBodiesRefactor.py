2dd02dde4b1cd3b2c52ba646ff1d468e8ad1152a
SaShaShady <sakshidoshi3@gmail.com>
2023-05-13 16:01:53 -0400

    Added code for Reduction category for Refactoring for RQ2.

--- Python/ClassifyIssueBodiesRefactor.py
+++ Python/ClassifyIssueBodiesRefactor.py
@@ -9,18 +9,21 @@ with open(file='extractedIssueDescRefactoring.txt', mode='r') as out_file:
 list1 =["Add","add","append","addition","creation","create","apply","clean",
     "code optimization","extend","introduce"]
 list2=["clean","modify","repair","fix","simplify","split","redesign","refactor","rewrite","repackage"]
+list3=["reduce","reduction","delete","subtract","remove","dump","rid","push down","redundant"]
 counterAdd =0
 counterModify=0
 totalIssues = 0
+counterDelete = 0
 for i in lines:
   totalIssues+=1
-  
   j = i.split()
   for m in j:
     if m.lower() in list1:
       counterAdd+=1
     if m.lower() in list2:
         counterModify+=1
+    if m.lower() in list3:
+        counterDelete+=1
 print("total",totalIssues)
-print(counterAdd,counterModify)
+print(counterAdd,counterModify,counterDelete)
 
