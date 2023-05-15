f2c7d2797680441d58ea198002371fb3718d0054
PYee1999 <prestonkyee@gmail.com>
2023-05-15 15:24:00 -0400

    Created extractIssueDesc() in ClassifyIssueBodiesBugs.py to fix extracting list error

--- Python/ClassifyIssueBodiesBugs.py
+++ Python/ClassifyIssueBodiesBugs.py
@@ -1,16 +1,48 @@
 import re
-lines = []
-with open(file='extractedIssueDescBugs.txt', mode='r') as out_file:
-        # Go through every line in file
-        for line in out_file:
-            # If line contains "Issue Description:", that is the start of a new issue body; reset flag to false
-            if "Issue Description:" in line:
-                lines.append(line)
+
+
+def extractIssueDesc(filepath: str) -> list:
+    '''
+    Extract Issue Descriptions as format of 
+    :param filepath: path to txt file
+    Returns:
+    '''
+    lines = []
+    
+    # Open file
+    with open(file=filepath, mode='r') as out_file:
+            # Have empty string for issue description
+            iss_desc_entry = ""
+
+            # Go through every line in file
+            for line in out_file:
+                # If line contains "Issue Description:", that is the start of a new issue body; reset flag to false
+                if "Issue Description:" in line:
+                    if len(iss_desc_entry) == 0:
+                        # Add first line to entry
+                        iss_desc_entry += line
+                    else:
+                        # Remove any newlines \n. End of current entry; add to list
+                        lines.append(iss_desc_entry.replace("\n", " "))
+
+                        # Update entry with next entry
+                        iss_desc_entry = line
+                else:
+                    # Keep adding line to entry
+                    iss_desc_entry += line
+
+    # Return list
+    return lines
+
+lines = extractIssueDesc(filepath='./Python/extractedIssueDescBugs.txt')
+print(lines[0:5])
+exit()
 
 list1 =["found a bug", "there is a bug", "a bug has been found", "it is a bug", "this is a bug", "this bug", "catch(ed) this/a bug"]
 list2=["bug#"]
 list3=["fixed a bug", "bug fix(es)", "fixes bug", "fix bug"]
 list4=["bug report(ed)", "report a bug","bug report"]
+
 counterFound =0
 counterBugClassify=0
 totalIssues = 0
