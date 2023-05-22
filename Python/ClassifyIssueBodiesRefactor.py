84118e8e5ce0e74ac297c5a6332ce14b9fc23209
Saakshaat <saakshaat2001@gmail.com>
2023-05-22 18:26:27 -0400

    revert the commit reverting ClassifyIssueBodiesRefactor

--- Python/ClassifyIssueBodiesRefactor.py
+++ Python/ClassifyIssueBodiesRefactor.py
@@ -1,3 +1,6 @@
+import re
+import numpy as np
+import matplotlib.pyplot as plt
 lines = []
 with open(file='extractedIssueDescRefactoring.txt', mode='r') as out_file:
         # Go through every line in file
@@ -5,30 +8,164 @@ with open(file='extractedIssueDescRefactoring.txt', mode='r') as out_file:
             # If line contains "Issue Description:", that is the start of a new issue body; reset flag to false
             if "Issue Description:" in line:
                 lines.append(line)
+add_verbs = [
+    "add",
+    "append",
+    "creat",
+    "introduc",
+    "nice to hav",
+    "creat",
+    "helpful"
+]
 
-list1 =["Add","add","append","addition","creation","create","apply","clean",
-    "code optimization","extend","introduce"]
-list2=["clean","modify","repair","fix","simplify","split","redesign","refactor","rewrite","repackage","refactor", "refactoring", "refactors", "refactorings", "refactored"]
-list3=["reduce","reduction","delete","subtract","remove","dump","rid","push down","redundant"]
+modify_verbs = [
+    "clean",
+    "modify",
+    "chang",
+    "mov",
+    "repair",
+    "fix",
+    "simplif",
+    "split",
+    "refactor",
+    "rewrit",
+    "reorganiz",
+    "shift",
+    "migrat",
+    "extend",
+    "improve",
+    "restructur",
+    "consolidat",
+    "decompos",
+    "merg",
+    "extract",
+    "moderniz",
+    "renam",
+    "reorder",
+    "updat",
+    "format",
+    "combin",
+    "optimiz",
+    "optimis"
+]
+
+delete_verbs = [
+    "delet",
+    "reduc",
+    "remov",
+    "rid",
+    "dump",
+    "scratch",
+    "redundan",
+    "eliminat",
+    "drop",
+    "discard",
+    "exclud",
+    "prun",
+    "trim"
+]
+
+nouns = [
+    "feat",
+    "func",
+    "class",
+    "component",
+    "method",
+    "property",
+    "parameter",
+    "arg",
+    "interface",
+    "dependenc",
+    "lib",
+    "plugin",
+    "test",
+    "coverage",
+    "example",
+    "demo",
+    "doc",
+    "trace",
+    "error.*message",
+    "error.*msg",
+    "warning",
+    "order",
+    "performance",
+    "style",
+    "versioning",
+    "format",
+    "readability",
+    "comment"
+]
+
+add_list = [f"{verb}.*{noun}" for verb in add_verbs for noun in nouns] + [f"{noun}.*{verb}" for verb in add_verbs for noun in nouns]
+
+modify_list = [f"{verb}.*{noun}" for verb in modify_verbs for noun in nouns] + [f"{noun}.*{verb}" for verb in modify_verbs for noun in nouns]
+
+delete_list = [f"{verb}.*{noun}" for verb in delete_verbs for noun in nouns] + [f"{noun}.*{verb}" for verb in delete_verbs for noun in nouns]
+
+#add_list =[
+#    "add",
+#    "append",
+#    "creat",
+#    "apply",
+#    "extend",
+#    "introduce"
+#]
+#modify_list=["clean","modify","repair","fix","simplify","split","redesign","refactor","rewrite","repackage","refactor", "refactoring", "refactors", "refactorings", "refactored","cleaned","simplified"]
+#delete_list=["reduce","reduction","delete","subtract","remove","dump","rid","push down","redundant"]
 counterAdd =0
 counterModify=0
 totalIssues = 0
 counterDelete = 0
+
 for i in lines:
-  totalIssues+=1
-  j = i.split()
-  for m in j:
-    if any(word in m.lower() for word in list1):
-      counterAdd+=1
-    elif any(word in m.lower() for word in list2):
-        counterModify+=1
-    elif any(word in m.lower() for word in list3):
-        counterDelete+=1
-#print("total",totalIssues)
-#print(counterAdd,counterModify,counterDelete)
+    totalIssues+=1
+    if any(re.search(r"\b{}\b".format(word), i, re.IGNORECASE) for word in delete_list):
+        counterDelete += 1
+    elif any(re.search(r"\b{}\b".format(word), i, re.IGNORECASE) for word in modify_list):
+        counterModify += 1
+    elif any(re.search(r"\b{}\b".format(word), i, re.IGNORECASE) for word in add_list):
+        counterAdd += 1
+
+print("total",totalIssues)
+print(f"Issues about additions: {counterAdd}")
+print(f"Issues about modifications: {counterModify}")
+print(f"Issues about deletions: {counterDelete}")
 
 print("Evaluation Criteria")
 print("Issues that talked about Code Addition: ",(counterAdd/totalIssues)*100,"%")
 print("Issues that talked about Code Modification: ",(counterModify/totalIssues)*100,"%")
 print("Issues that talked about Code Deletion: ",(counterDelete/totalIssues)*100,"%")
+
+d = np.array([counterAdd, counterModify, counterDelete, totalIssues])
+myexplode = [0.2,0]
+fig1, ax1 = plt.subplots()
+plotLabel = ["Issues about Code Addition", "Issues About Code Modification", "Issues About Code Deletion", "Other Issues"]
+plt.pie(d, labels=plotLabel,autopct='%1.1f%%', startangle=90)
+plt.axis('equal')
+plt.show()
+
+d = np.array([counterAdd, totalIssues])
+myexplode = [0.2,0]
+fig1, ax1 = plt.subplots()
+plotLabel = ["Issues about Code Addition","Other Issues"]
+plt.pie(d, labels=plotLabel,explode = myexplode,autopct='%1.1f%%', startangle=90)
+plt.axis('equal')
+plt.show()
+
+d = np.array([counterModify, totalIssues])
+myexplode = [0.2,0]
+fig1, ax1 = plt.subplots()
+plotLabel = ["Issues about Code Modification","Other Issues"]
+plt.pie(d, labels=plotLabel,explode = myexplode,autopct='%1.1f%%', startangle=90)
+plt.axis('equal')
+plt.show()
+
+d = np.array([counterDelete, totalIssues])
+myexplode = [0.2,0]
+fig1, ax1 = plt.subplots()
+plotLabel = ["Issues about Code Deletion","Other Issues"]
+plt.pie(d, labels=plotLabel,explode = myexplode,autopct='%1.1f%%', startangle=90)
+plt.axis('equal')
+plt.show()
+
 
