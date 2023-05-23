972eab100f27d302cdac3cc091f07658c86bfe01
SaShaShady <sakshidoshi3@gmail.com>
2023-05-22 23:50:48 -0400

    Commiting  final changes to the repo.

--- Python/ManualValidation.py
+++ Python/ManualValidation.py
@@ -1,6 +1,8 @@
 import PythonApplicationForBugs as bugs
 import PythonApplicationForRefactoring as refactor
 import random
+import numpy as np
+import matplotlib.pyplot as plt
 
 def checkRefactoringTitles():
     print(random.choices(refactor.refactoringIssueTitles, k=20))
@@ -27,9 +29,46 @@ def checkBugsBodies():
             if "Issue Description:" in line:
                 lines2.append(line)
     print(random.choices(lines2, k=20))
+
     
 checkRefactoringTitles()
+print("+++++++=")
 checkBugsTitles()
+print("+++++++=")
 checkRefactoringBodies()
+print("+++++++=")
 checkBugsBodies()
+print("+++++++=")
 
+
+d = np.array([1, 20])
+myexplode = [0.2,0]
+fig1, ax1 = plt.subplots()
+plotLabel = ["Incorrect Refactor Issue Title Classification","Correct Classification"]
+plt.pie(d, labels=plotLabel,explode = myexplode,autopct='%1.1f%%', startangle=90)
+plt.axis('equal')
+plt.show()
+
+d = np.array([2, 20])
+myexplode = [0.2,0]
+fig1, ax1 = plt.subplots()
+plotLabel = ["Incorrect Bug Issue Title Classification","Correct Classification"]
+plt.pie(d, labels=plotLabel,explode = myexplode,autopct='%1.1f%%', startangle=90)
+plt.axis('equal')
+plt.show()
+
+d = np.array([2, 20])
+myexplode = [0.2,0]
+fig1, ax1 = plt.subplots()
+plotLabel = ["Total incorrect classification for Refactoring Issue Bodies","Correct Classification"]
+plt.pie(d, labels=plotLabel,explode = myexplode,autopct='%1.1f%%', startangle=90)
+plt.axis('equal')
+plt.show()
+
+d = np.array([3, 20])
+myexplode = [0.2,0]
+fig1, ax1 = plt.subplots()
+plotLabel = ["Total incorrect classification for Bugs Issue Bodies","Correct Classification"]
+plt.pie(d, labels=plotLabel,explode = myexplode,autopct='%1.1f%%', startangle=90)
+plt.axis('equal')
+plt.show()
